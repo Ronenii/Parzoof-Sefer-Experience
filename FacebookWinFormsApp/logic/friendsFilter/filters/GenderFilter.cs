43fa0d059188152b37584c889950e35190ea15d0
Roy <roytol12@gmail.com>
2024-01-21 17:40:07 +0200

    push

---
+++ FacebookWinFormsApp/logic/friendsFilter/filters/GenderFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures.logic.friendsFilter.filters
+{
+    public enum e_Gender
+    {
+        Male,
+        Female
+    }
+
+    public class GenderFilter
+    {
+        private readonly e_Gender m_Gender;
+
+        public GenderFilter(e_Gender i_Gender)
+        {
+            m_Gender = i_Gender;
+        }
+    }
+}
