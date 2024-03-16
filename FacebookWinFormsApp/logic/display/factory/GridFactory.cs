154f0fa1ec4a7e4ac751c39ec380e57c44c9edbf
Roy <roytol12@gmail.com>
2024-03-16 17:43:34 +0200

    some changes

---
+++ FacebookWinFormsApp/logic/display/factory/GridFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicFacebookFeatures.logic.grid;
+using FacebookWrapper.ObjectModel;
+using System.Reflection;
+using BasicFacebookFeatures.session;
+
+namespace BasicFacebookFeatures.logic.display.factory
+{
+    public static class GridFactory<T>
+    {
+        public static FacebookObjectDisplayGrid<T> CreateGrid()
+        {
+            FacebookObjectDisplayGrid<T> res = null;
+
+            switch (typeof(T).ToString())
+            {
+                case "Album":
+                    res = new FacebookObjectDisplayGrid<T>(UserWrapper.GetAlbums);
+                    break;
+                case "User":
+                    res = new FacebookObjectDisplayGrid<T>(UserWrapper.GetFriends);
+                    break;
+                case "Page":
+                    res = new FacebookObjectDisplayGrid<T>(UserWrapper.GetLikedPages);
+                    break;
+            }
+
+            return res;
+        }
+    }
+}
